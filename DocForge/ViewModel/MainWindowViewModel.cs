27d4ae67bc60b038c11e107aa64a5fa5dda05ed2
alex.vorobiev <a.vorobiev@rheagroup.com>
2015-05-18 11:18:05 +0200

    Some progress on VM

---
+++ DocForge/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindowViewModel.cs" company="Red Hammer Studios">
+//   Copyright (c) 2015 Red Hammer Studios
+// </copyright>
+// <summary>
+//   The main window view model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.IO;
+using System.Reflection;
+using ClassForge.Model;
+
+namespace DocForge.ViewModel
+{
+    using ReactiveUI;
+
+    /// <summary>
+    /// The main window view model.
+    /// </summary>
+    public class MainWindowViewModel : ReactiveObject
+    {
+        private string folderPath;
+
+        private Model fullModel;
+
+        private Model filteredModel;
+
+        public string FolderPath
+        {
+            get { return this.folderPath; }
+            set { this.RaiseAndSetIfChanged(ref this.folderPath, value); }
+        }
+
+        public Model FullModel
+        {
+            get { return this.fullModel; }
+            set { this.RaiseAndSetIfChanged(ref this.fullModel, value); }
+        }
+
+        public Model FilteredModel
+        {
+            get { return this.filteredModel; }
+            set { this.RaiseAndSetIfChanged(ref this.filteredModel, value); }
+        }
+
+        public ReactiveCommand<object> BrowseFolderCommand { get; private set; }
+
+        public ReactiveCommand<object> ParseCommand { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            // initialize commands
+            this.SetProperties();
+        }
+
+        private void SetProperties()
+        {
+#if DEBUG
+            this.folderPath = Directory.GetCurrentDirectory() + "\\mergetest";
+#endif
+        }
+
+        public void BrowseFolderCommandExecute()
+        {
+            // Do a folder picker and assign to the textbox
+        }
+
+        public void ParseCommandExecute()
+        {
+            // Call on ClassForge
+        }
+    }
+}
